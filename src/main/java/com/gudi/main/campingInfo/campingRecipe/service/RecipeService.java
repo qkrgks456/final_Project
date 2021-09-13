7b7ad9c6817972193ffcc81001f99e29fe63dcd8
Chano <cowhd96@naver.com>
2021-09-13 18:19:49 +0900

    종우 관리자

--- src/main/java/com/gudi/main/campingInfo/campingRecipe/service/RecipeService.java
+++ src/main/java/com/gudi/main/campingInfo/campingRecipe/service/RecipeService.java
@@ -1,7 +1,18 @@
 package com.gudi.main.campingInfo.campingRecipe.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gudi.main.campingInfo.campingRecipe.dao.RecipeMapper;
 
 @Service
 public class RecipeService {
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired RecipeMapper dao;
+	public void test () {		
+		int a=dao.test();
+		logger.info("f:"+a);
+	}
 }
